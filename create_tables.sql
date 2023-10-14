07aaa85ce4b91a334ba92f675fe280e293a6dbb0
David Palensky <david.palensky@gmail.com>
2023-10-14 16:53:46 +0200

    Created db tables.

--- create_tables.sql
+++ create_tables.sql
@@ -1,17 +1,30 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL
+);
+
+
 CREATE TABLE tags (
     tag_id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL
+    title TEXT NOT NULL,
+    color TEXT NOT NULL
 );
 
 CREATE TABLE tasks (
     task_id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP
+    creation INTEGER DEFAULT (unixepoch('now')),
+    deadline INTEGER DEFAULT (unixepoch('now')),
+    completed BOOLEAN DEFAULT 0,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-// Not yet implemented.
-CREATE TABLE tag_task_links (
+CREATE TABLE task_tag_links (
+    task_id INTEGER,
+    tag_id INTEGER,
     FOREIGN KEY (task_id) REFERENCES tasks (task_id),
     FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
 );
+
