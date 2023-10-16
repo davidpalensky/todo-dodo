d774907dc3c3ba765266238430fa0e8cb99815f8
David Palensky <david.palensky@gmail.com>
2023-10-16 14:00:31 +0200

    More docs

--- db/db.go
+++ db/db.go
@@ -8,8 +8,11 @@ import (
 	_ "github.com/jmoiron/sqlx"
 )
 
+// The database connection
 var DB *sqlx.DB
 
+// Connect to database, should only be called once.
+// Make sure environment variables "TODO_DODO_DB_URL" and "TODO_DODO_DB_TOKEN" are set correctly.
 func DBConnect() {
 	// url should be in the format: libsql://{database}.turso.io?authToken={auth token}
 	url := os.Getenv("TODO_DODO_DB_URL")
