c84b73a419245256f3ae444118aece62efe36472
David Palensky <david.palensky@gmail.com>
2023-10-21 13:59:40 +0200

    Added frontend functionality for deleting tasks. Pray for js to bless us.

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ function taskToggleCompleted(checkbox) {
     console.log("Updating task completed status.")
     console.log(update_args)
 
-    let url = "http://" + self.location.host + "/api/v1/task/update"
+    let url = "http:\/\/" + self.location.host + "/api/v1/task/update"
     fetch(url, {
         method: "POST",
         body: update_args,
@@ -18,4 +18,31 @@ function taskToggleCompleted(checkbox) {
             "Content-type": "application/json; charset=UTF-8"
         }
     })
+}
+
+// taskDelete conducts an API req to delete that task, and removes it form the DOM
+function taskDelete(domIdOfElemOfEntireTask) {
+    let dom_id = domIdOfElemOfEntireTask
+    let task_id = parseInt(checkbox.id.match(/[0-9]+/g));
+    // Task element
+    const e = document.getElementById(dom_id)
+
+    let delete_args = JSON.stringify({
+        taskids: [task_id]
+    });
+    console.log("Deleting task.");
+    console.log(delete_args);
+
+    let url = "http:\/\/" + self.location.host + "/api/v1/task/delete";
+    let response = Promise.resolve(fetch(url, {
+        method: "POST",
+        body: delete_args,
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }));
+    console.log(response);
+
+    // Remove element, lets hope this is like correct and shit
+    dom_id.remove();
 }
