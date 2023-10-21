e2e6d78f5d6edb7704002aa9c422e7708e19996d
David Palensky <david.palensky@gmail.com>
2023-10-21 15:14:46 +0200

    Clean up routing.

--- todo-dodo.go
+++ todo-dodo.go
@@ -22,23 +22,24 @@ func main() {
 	engine.Use(gin.Recovery())
 
 	// JSON-based APIs
-	engine.POST("/api/v1/task/create", api.TaskCreateBatchEnpoint)
-	engine.POST("/api/v1/task/fetch", api.TaskFetchAllEnpoint)
-	engine.POST("/api/v1/task/delete", api.TaskDeleteBatchEnpoint)
-	engine.POST("/api/v1/task/update", api.TaskUpdateEndpoint)
+	api_g := engine.Group("/api/v1")
+	api_g.POST("/task/create", api.TaskCreateBatchEnpoint)
+	api_g.POST("/task/fetch", api.TaskFetchAllEnpoint)
+	api_g.POST("/task/delete", api.TaskDeleteBatchEnpoint)
+	api_g.POST("/task/update", api.TaskUpdateEndpoint)
 
-	engine.POST("/api/v1/tag/delete", api.TagDeleteBatchEnpoint)
+	api_g.POST("/tag/delete", api.TagDeleteBatchEnpoint)
 
-	// Web Pages
+	// Load Templates
 	engine.LoadHTMLGlob("./templates/*")
 
+	// Make Templates avaliable
 	engine.GET("/", pages.Index)
-
 	if os.Getenv("TODO_DODO_DEV") == "1" {
 		engine.GET("/test.html", pages.Test)
 	}
 
-	// Js files for client side behaviour
+	// Js files
 	engine.StaticFile("index.js", "./pages/index.js")
 
 	// Run server
