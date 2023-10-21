e4a653814e3db117a0862076d996a830fa8a3d3c
David Palensky <david.palensky@gmail.com>
2023-10-21 14:33:38 +0200

    Move env vars into script files. Simplification.

--- dev.sh
+++ dev.sh
@@ -1,2 +1,3 @@
-source dev.env
+export CGO_ENABLED=1
+export TODO_DODO_DEV=1
 go run todo-dodo.go
--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
-source release.env
+export CGO_ENABLED=1
+export GIN_MODE="release"
+export TODO_DODO_DEV=0
 go run todo-dodo.go
