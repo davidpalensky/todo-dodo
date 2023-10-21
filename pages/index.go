03f6628b3fb841272b08ca00254a21c34b296492
David Palensky <david.palensky@gmail.com>
2023-10-21 23:54:47 +0200

    Fun stuff

--- pages/index.go
+++ pages/index.go
@@ -13,11 +13,14 @@ import (
 // Golang, what about #[allow(unused_imports)]?
 var _ = log.Printf
 
-type TaskView struct {
-	Title     string
-	Deadline  string
-	Completed bool
-	Task_id   uint64
+type Task struct {
+	Title        string
+	Content      string
+	Creation     uint64
+	Deadline     uint64
+	Deadline_fmt string
+	Completed    bool
+	Task_id      uint64
 }
 
 // Generate index file and fill in the data
@@ -34,18 +37,21 @@ func Index(ctx *gin.Context) {
 		return data.Tasks[i].Task_data.Deadline < data.Tasks[j].Task_data.Deadline
 	})
 
-	var task_view []TaskView
+	var tasks []Task
 	for i := 0; i < len(data.Tasks); i++ {
-		task_view = append(task_view, TaskView{
-			Title:     data.Tasks[i].Task_data.Title,
-			Deadline:  time.Unix(int64(data.Tasks[i].Task_data.Deadline), 0).Format("2 Jan 2006"),
-			Completed: bool(data.Tasks[i].Task_data.Completed),
-			Task_id:   data.Tasks[i].Task_data.Task_id,
+		tasks = append(tasks, Task{
+			Title:        data.Tasks[i].Task_data.Title,
+			Content:      data.Tasks[i].Task_data.Content,
+			Creation:     data.Tasks[i].Task_data.Creation,
+			Deadline:     data.Tasks[i].Task_data.Deadline,
+			Deadline_fmt: time.Unix(int64(data.Tasks[i].Task_data.Deadline), 0).Format("2 Jan 2006"),
+			Completed:    bool(data.Tasks[i].Task_data.Completed),
+			Task_id:      data.Tasks[i].Task_data.Task_id,
 		})
 	}
 	//log.Println("view: ", task_view)
 
 	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"Tasks": task_view,
+		"Tasks": tasks,
 	})
 }
