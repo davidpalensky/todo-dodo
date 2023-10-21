830525b501b3529537d4ca86db6e1e226ed97a2e
David Palensky <david.palensky@gmail.com>
2023-10-21 15:05:30 +0200

    Define port explicitely.

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 export CGO_ENABLED=1
 export TODO_DODO_DEV=1
+export PORT=8080
 go run todo-dodo.go
--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 export CGO_ENABLED=1
 export GIN_MODE="release"
 export TODO_DODO_DEV=0
+export PORT=8080
 go run todo-dodo.go
